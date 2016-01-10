fa774cd9881a0dae847002002d465fdcbeff85f5
Apple <Apple@Apples-MacBook-Air.local>
2016-01-10 19:17:05 +0700

    Initial Commit

---
+++ UITableView3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView3
+//
+//  Created by Apple on 1/10/16.
+//  Copyright (c) 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableView3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView3
+//
+//  Created by Apple on 1/10/16.
+//  Copyright (c) 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
